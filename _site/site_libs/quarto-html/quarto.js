b51a63dca34c051b1c642c10026dddf9d0342e01
DrDeception <g.wright@gold.ac.uk>
2022-10-12 13:59:10 +0100

    desktop3

--- _site/site_libs/quarto-html/quarto.js
+++ _site/site_libs/quarto-html/quarto.js
@@ -12,7 +12,6 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
   const marginSidebarEl = window.document.getElementById(
     "quarto-margin-sidebar"
   );
-
   // function to determine whether the element has a previous sibling that is active
   const prevSiblingIsActiveLink = (el) => {
     const sibling = el.previousElementSibling;
@@ -22,6 +21,17 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
       return false;
     }
   };
+
+  // fire slideEnter for bootstrap tab activations (for htmlwidget resize behavior)
+  function fireSlideEnter(e) {
+    const event = window.document.createEvent("Event");
+    event.initEvent("slideenter", true, true);
+    window.document.dispatchEvent(event);
+  }
+  const tabs = window.document.querySelectorAll('a[data-bs-toggle="tab"]');
+  tabs.forEach((tab) => {
+    tab.addEventListener("shown.bs.tab", fireSlideEnter);
+  });
 
   // Track scrolling and mark TOC links as active
   // get table of contents and sidebar (bail if we don't have at least one)
@@ -60,7 +70,11 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
 
   const sections = tocLinks.map((link) => {
     const target = link.getAttribute("data-scroll-target");
-    return window.document.querySelector(decodeURI(`${target}`));
+    if (target.startsWith("#")) {
+      return window.document.getElementById(decodeURI(`${target.slice(1)}`));
+    } else {
+      return window.document.querySelector(decodeURI(`${target}`));
+    }
   });
 
   const sectionMargin = 200;
@@ -143,8 +157,13 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
   function offsetAbsoluteUrl(url) {
     const offset = getMeta("quarto:offset");
     const baseUrl = new URL(offset, window.location);
+
     const projRelativeUrl = url.replace(baseUrl, "");
-    return "/" + projRelativeUrl;
+    if (projRelativeUrl.startsWith("/")) {
+      return projRelativeUrl;
+    } else {
+      return "/" + projRelativeUrl;
+    }
   }
 
   // read a meta tag value
@@ -159,15 +178,26 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
   }
 
   async function findAndActivateCategories() {
-    const thisPath = window.location.pathname;
+    const currentPagePath = offsetAbsoluteUrl(window.location.href);
     const response = await fetch(offsetRelativeUrl("listings.json"));
     if (response.status == 200) {
       return response.json().then(function (listingPaths) {
         const listingHrefs = [];
         for (const listingPath of listingPaths) {
+          const pathWithoutLeadingSlash = listingPath.listing.substring(1);
           for (const item of listingPath.items) {
-            if (item === thisPath || item === thisPath + "index.html") {
-              listingHrefs.push(listingPath.listing);
+            if (
+              item === currentPagePath ||
+              item === currentPagePath + "index.html"
+            ) {
+              // Resolve this path against the offset to be sure
+              // we already are using the correct path to the listing
+              // (this adjusts the listing urls to be rooted against
+              // whatever root the page is actually running against)
+              const relative = offsetRelativeUrl(pathWithoutLeadingSlash);
+              const baseUrl = window.location;
+              const resolvedPath = new URL(relative, baseUrl);
+              listingHrefs.push(resolvedPath.pathname);
               break;
             }
           }
@@ -243,13 +273,9 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
 
         // Converts the sidebar to a menu
         const convertToMenu = () => {
-          const elBackground = window
-            .getComputedStyle(window.document.body, null)
-            .getPropertyValue("background");
-          el.classList.add("rollup");
-
           for (const child of el.children) {
             child.style.opacity = 0;
+            child.style.display = "none";
           }
 
           const toggleContainer = window.document.createElement("div");
@@ -277,7 +303,6 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
           toggleContainer.append(toggleTitle);
 
           const toggleContents = window.document.createElement("div");
-          toggleContents.style.background = elBackground;
           toggleContents.classList = el.classList;
           toggleContents.classList.add("zindex-over-content");
           toggleContents.classList.add("quarto-sidebar-toggle-contents");
@@ -288,6 +313,7 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
 
             const clone = child.cloneNode(true);
             clone.style.opacity = 1;
+            clone.style.display = null;
             toggleContents.append(clone);
           }
           toggleContents.style.height = "0px";
@@ -352,6 +378,7 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
         const convertToSidebar = () => {
           for (const child of el.children) {
             child.style.opacity = 1;
+            clone.style.display = null;
           }
 
           const placeholderEl = window.document.getElementById(
@@ -388,38 +415,23 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
     };
   };
 
-  const offsetEl = window.document.querySelector(
-    `*[data-sidebar-align="true"]`
+  // Find any conflicting margin elements and add margins to the
+  // top to prevent overlap
+  const marginChildren = window.document.querySelectorAll(
+    ".column-margin.column-container > * "
   );
-  let offsetTopPadding = null;
-  const positionSidebars = () => {
-    if (offsetEl !== null) {
-      if (offsetTopPadding === null) {
-        offsetTopPadding = offsetEl.style.paddingTop;
-      }
-      const rect = offsetEl.getBoundingClientRect();
-      // subtract any headroom offiset, if present
-      const position = Math.max(rect.height, 0);
-
-      const floating = window.document.querySelector("body.floating");
-      const sidebarIds = ["quarto-margin-sidebar", "quarto-sidebar-toc-left"];
-      if (floating) {
-        sidebarIds.push("quarto-sidebar");
-      }
-      sidebarIds.forEach((sidebarId) => {
-        const sidebarEl = window.document.getElementById(sidebarId);
-        if (sidebarEl) {
-          sidebarEl.style.marginTop = `${position}px`;
-          if (position > 0) {
-            sidebarEl.style.paddingTop = "0.5em";
-          } else {
-            sidebarEl.style.paddingTop = offsetTopPadding;
-          }
-        }
-      });
+  let lastBottom = 0;
+  for (const marginChild of marginChildren) {
+    const top = marginChild.getBoundingClientRect().top;
+    if (top < lastBottom) {
+      const margin = lastBottom - top;
+      marginChild.style.marginTop = `${margin}px`;
     }
-  };
-  positionSidebars();
+    const styles = window.getComputedStyle(marginChild);
+    const marginTop = parseFloat(styles["marginTop"]);
+
+    lastBottom = top + marginChild.getBoundingClientRect().height + marginTop;
+  }
 
   // Manage the visibility of the toc and the sidebar
   const marginScrollVisibility = manageSidebarVisiblity(marginSidebarEl, {
@@ -621,7 +633,6 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
     "scroll",
     throttle(() => {
       if (tocEl) {
-        positionSidebars();
         updateActiveLink();
         walk(tocEl, 0);
       }
@@ -633,8 +644,6 @@ window.document.addEventListener("DOMContentLoaded", function (_event) {
   window.addEventListener(
     "resize",
     throttle(() => {
-      positionSidebars();
-
       if (!isReaderMode()) {
         hideOverlappedSidebars();
       }
